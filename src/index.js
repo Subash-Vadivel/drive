00579bbdda273f58a499f431846d6b3acd8cf1a1
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2022-08-24 18:45:05 +0530

    v

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Home from './Home';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
