06f42e7a0996895fa37d5fff50e0c06054997bd5
vishnukumar07 <vishnuk55265@gmail.com>
2022-08-24 19:04:36 +0530

    s5

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,21 @@
 import { Col, Row } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import Header from './resources/components/Header';
 function Login()
 {
-  return (<>
-    
+  return (<>{Header()}
+
+    <div className='background'>
+      <Container>
+        
+          <form>
+            <label>username:</label><input type='text'></input><br />
+
+            <label>password:</label><input type='text'></input><br />
+          </form>
+          
+      </Container>
+  </div>
     
     
   </>
