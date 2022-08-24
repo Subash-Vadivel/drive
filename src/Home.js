2922c69106a6bd7a1ba7b654b62284bb839b7aa2
vishnukumar07 <vishnuk55265@gmail.com>
2022-08-24 18:38:12 +0530

    s4

--- src/Home.js
+++ src/Home.js
@@ -125,11 +125,11 @@ function Menu()
 {
 return(<><Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Rid</Navbar.Brand>
+        <Navbar.Brand href="#home">Rido</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="justify-content-end" style={{ width: "100%" }}>
-          <Nav.Link href="/home">Lo</Nav.Link>
+          <Nav.Link href="/home">Login</Nav.Link>
           <Nav.Link eventKey="link-1">Signup</Nav.Link>
         </Nav>
         </Navbar.Collapse>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './Home';
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    
+    <Login />
   </React.StrictMode>
 );
 
