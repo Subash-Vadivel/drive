36b5f92076a9bc34019cde4e1484d3bde07e4a6f
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2022-08-25 14:25:36 +0530

    s12

--- src/resources/components/Header.js
+++ src/resources/components/Header.js
@@ -1,8 +1,19 @@
 import '../css/header.css';
+import logo from '../images/3.png';
+import profile from '../images/profile.webp';
 function Header(props)
 {
-   return(<nav>
-            
-   </nav> );
+   return(
+      <nav className="nav-bar">
+      <div className="logo-wrapper">
+             <img src={logo} alt="logo"/>
+      </div>
+      <div className="profile-wrapper">
+            <div className="profile-name">Hello, Subash</div>
+            <div className="profile-pic"><img src={profile} alt="profile"/></div>
+      </div>
+</nav>
+
+    );
 }
 export default Header;
