2214be2b0f384bcfe168fe90141e2e88f9b19d41
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2022-08-29 17:48:20 +0530

    s20

--- src/resources/components/Footer.js
+++ src/resources/components/Footer.js
@@ -1,15 +1,45 @@
 import '../css/Footer.css';
 import { Row,Col,Container } from 'react-bootstrap';
+import logofoot from '../images/newlogo2.png';
 
 function Footer()
 {
     return(<>
-    <Container>
-        <Row>
-            <Col xs={3}>About Rido</Col>
-            <Col xs={3}>Info</Col>
-            <Col xs={6}>Availability</Col>
+    <Container className='footer-top'>
+        <span className='footer-top-item'><img src={logofoot} alt="..."/></span>
+    </Container>
+    <Container fluid className='footer-main'>
+        <Row className='foot-row'>
+            <Col xs={3}><ul type="none" className='foot-li'>
+                <li>About Rido</li>
+                <li>Home</li>
+                <li>Contact Us</li>
+                <li>Offers</li>
+                <li>Support</li>
+                <li>Careers</li>
+                <li>Blog</li>
+                </ul></Col>
+            <Col xs={3}>
+                <ul type="none" className='foot-li'>
+                    <li>Info</li>
+                    <li>Fares</li>
+                    <li>Business Partner</li>
+                    <li>Become a Driver</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conditions</li>
+                    {/* &#38; */}
+                </ul>
+            </Col>
+            <Col xs={6}>
+            <ul type="none" className='foot-li'>
+                <li>Availability</li>
+                <li>We are in</li>
+                </ul>
+                </Col>
         </Row>
+    </Container>
+    <Container fluid className='footer-end'>
+        <p className='footer-end-txt'>© 2022 Team Alpha. All rights reserved.</p>
     </Container>
     </>);
 }
