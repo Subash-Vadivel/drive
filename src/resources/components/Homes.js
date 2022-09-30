f0a7c6fd45af5e7f5cb4bbf0f9ee9c3cf29ecf6d
vishnukumar07 <vishnuk55265@gmail.com>
2022-09-30 14:03:15 +0530

    last

--- src/resources/components/Homes.js
+++ src/resources/components/Homes.js
@@ -1,16 +1,17 @@
-import Services from './Services';
-import Slider from './Slider';
-import Community from './Community';
-import Offers from './Offers';
-import Trip from './Triptypes';
-function Homes()
-{
-    return(<>  
-    <Slider/>
-    <Offers/>
-    <Trip/>
-    <Services/>
-    <Community/>
-    </>);
+import Services from "./Services";
+import Slider from "./Slider";
+import Community from "./Community";
+import Offers from "./Offers";
+import Trip from "./Triptypes";
+function Homes() {
+  return (
+    <>
+      <Slider />
+      <Offers />
+      <Trip />
+      {/* <Services/> */}
+      <Community />
+    </>
+  );
 }
 export default Homes;
