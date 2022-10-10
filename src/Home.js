ac180ab368a7b3dba2ab4415f8a8e18dbd4eeccc
vishnukumar07 <vishnuk55265@gmail.com>
2022-10-10 21:57:36 +0530

    last

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import './resources/css/App.css';
-import Header from './resources/components/Header';
-import Footer from './resources/components/Footer';
-import Homes from './resources/components/Homes';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./resources/css/App.css";
+import Header from "./resources/components/Header";
+import Footer from "./resources/components/Footer";
+import Homes from "./resources/components/Homes";
+import Partner from "./resources/components/Part";
 function Home(props) {
-  return (<>
-  <header>
-     <Header  handleLogout={props.handleLogout}/>
-  </header>
-  <main>
-    <Homes/>
-  </main>
-  <footer>
-    <Footer/>
-  </footer>
-  </>
+  return (
+    <>
+      <header>
+        <Header handleLogout={props.handleLogout} />
+      </header>
+      <main>
+        <Homes />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 export default Home;
